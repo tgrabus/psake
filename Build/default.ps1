fd5a1e4e364378552cb1f12551bacce1a82204fa
tgrabus@outlook.com <tgrabus@outlook.com>
2015-11-27 00:58:57 +0100

    add build project to repo

---
+++ Build/default.ps1
@@ -0,0 +1,19 @@
+﻿properties {
+	$testMessage = 'Executed Test!'
+	$compileMessage = 'Executed Compile!'
+	$cleanMessage = 'Executed Clean!'
+}
+
+task default -depends Test
+
+task Clean {
+	Write-Host $cleanMessage
+}
+
+task Compile -depends Clean {
+	Write-Host $compileMessage
+}
+
+task Test -depends Compile, Clean {
+	Write-Host $testMessage
+}
