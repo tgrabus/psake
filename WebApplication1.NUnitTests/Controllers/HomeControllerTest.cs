5eb8035153543c37cf9f0b2b7233a6711d47febc
tgrabus@outlook.com <tgrabus@outlook.com>
2015-11-28 01:46:37 +0100

    add test project

---
+++ WebApplication1.NUnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.NUnitTests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
